3ef5ee3dca57a228ef9942deb5ea238df23ebe4d
1eif <leifliuo@gmail.com>
2023-07-17 17:31:44 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import sys
+from memos import memos
+
+from os import path
+
+from bs4 import BeautifulSoup
+
+
+
+def html_memo_to_json(html):
+
+    soup = BeautifulSoup(html, 'html.parser')
+    export_json = []
+
+    for item in soup.find_all(class_="memo"):
+
+        content = ""
+        files = []
+        selected = item.select(".content p, .content li")
+
+        for i, e in enumerate(selected):
+            text = e.get_text()
+
+            if i < len(selected) - 1:
+                text += "\n"
+
+            content += text
+
+        for e in item.select("img"):
+            src = e.get("src")
+            files.append(src)
+        export_json.append({
+            "content": content,
+            "filePath": files if files else None
+        })
+    return export_json
+
+
+def main(argv):
+
+    file_path = argv[0]
+    openapi_url = argv[1]
+    openId = argv[2]
+
+    memos_json = []
+
+    if file_path.endswith(".html"):
+        with open(file_path, "r", encoding='utf-8') as f:
+            html = f.read()
+
+            memos_json += html_memo_to_json(html)
+
+            print(f"finish: {len(memos_json)} data is exported")
+
+
+    print("=====================================")
+    print("start to import data to memos")
+    
+    memos_api = memos.Memos_api(openId, openapi_url)
+
+    for i, item in enumerate(memos_json):
+
+        memo = memos.Memos(content=item["content"], filePath=item["filePath"])
+
+        memos_api.create_memo(memo)
+
+        print("finish:", i + 1, "/", len(memos_json))
+
+    print("finish: all data is imported")
+
+if __name__ == "__main__":
+
+    main(sys.argv[1:])
+
+    
+
+
---
+++ memos/memos.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+import json
+import requests
+import re
+
+from urllib.parse import urljoin
+
+
+API_VERSION = "/api/v1/"
+
+# memos类
+class Memos:
+
+    def __init__(self, content:str, resourceIdList = [], relationList = [], visibility:str = "PRIVATE", filePath = [], fileUrl = []):
+        self.content = content
+        self.visibility = visibility
+        self.resourceIdList = resourceIdList
+        self.relationList = relationList
+        self.filePath = filePath
+        self.fileUrl = fileUrl
+
+    def get_tags(self):
+        reg = r"\#[^\s]+"
+        return re.findall(reg, self.content)
+
+    # def to_json(self):
+    #     return json.dumps(self, default=lambda o: o.__dict__, 
+    #         sort_keys=True, indent=4)
+    
+# memos相关API
+class Memos_api:
+
+    def __init__(self, openId, openapi_url):
+        self.openId = openId
+        self.openapi_url = openapi_url
+
+    def create_memo(self, memos:Memos):
+
+        if memos.filePath:
+            for file in memos.filePath:
+                res = self.upload_file(file)
+
+                if res:
+                    memos.resourceIdList.append(res)
+        
+        if memos.fileUrl:
+            for file in memos.fileUrl:
+                res = self.create_resource(file)
+
+                if res:
+                    memos.resourceIdList.append(res)
+        
+        tags = memos.get_tags()
+        if len(tags):
+            for tag in tags:
+                self.create_tag(tag)
+
+        url = urljoin(self.openapi_url, API_VERSION + "memo")
+        # url = self.openapi_url + "memo"
+        headers = {"Content-Type": "application/json"}
+        params = {"openId" : self.openId}
+        data = { "content": memos.content, "visibility": memos.visibility, "resourceIdList": memos.resourceIdList, "relationList": memos.relationList}
+
+        try:
+            res = requests.post(url, json=data, headers=headers, params=params)
+
+            match res.status_code:
+                case 200:
+                    print(200, "Created memo", json.loads(res.text)["content"])
+                    return True
+                case 400:
+                    print(400, "Invalid request")
+                    return False
+                case 401:
+                    print(401, "Unauthorized")
+                    return False
+                case 403:
+                    print(403, "Forbidden to create public memo")
+                    return False
+                case 500:
+                    print(500, "Internal server error")
+                    return False
+                case _:
+                    print("Unknown error")
+                    return False
+        except Exception as e:
+            print("create memo error:", e)
+            return False
+        
+
+    def create_tag(self, tag):
+        url = urljoin(self.openapi_url, API_VERSION + "tag")
+        # url = self.openapi_url + "tag"
+        headers = {"Content-Type": "application/json"}
+        params = {"openId" : self.openId}
+        data = {"name": tag.replace("#", "")}
+
+        try:
+            
+            res = requests.post(url, json=data, headers=headers, params=params)
+            
+            match res.status_code:
+                case 200:
+                    print(200, "Created tag", res.text)
+                    return True
+                case 400:
+                    print(400, "Invalid request")
+                    return False
+                case 500:
+                    print(500, "Internal server error")
+                    return False
+                case _:
+                    print("Unknown error")
+                    return False
+
+        except Exception as e:
+            print("create tag error:", e)
+            return None
+    
+    # 上传文件
+    # {
+    #   "id": 123,
+    #   "filename": "example.png"
+    #   // other fields
+    # }
+    def upload_file(self, filePath):
+        url = urljoin(self.openapi_url, API_VERSION + "resource/blob")
+        # url = self.openapi_url + "resource/blob"
+        params = {"openId" : self.openId}
+
+        # if filePath.split(".")[-1] == "png":
+        #     headers = {"Content-Type": "image/png"}
+        # elif filePath.split(".")[-1] == "jpg":
+        #     headers = {"Content-Type": "image/jpeg"}
+        # elif filePath.split(".")[-1] == "jpeg":
+        #     headers = {"Content-Type": "image/jpeg"}
+        # elif filePath.split(".")[-1] == "gif":
+        #     headers = {"Content-Type": "image/gif"}
+        # elif filePath.split(".")[-1] == "mp4":
+        #     headers = {"Content-Type": "video/mp4"}
+        # elif filePath.split(".")[-1] == "mp3":
+        #     headers = {"Content-Type": "audio/mp3"}
+        # else:
+        #     print("unknown file type")
+        #     headers = {"Content-Type": ""}
+
+        try:
+            files = {'file': open(filePath, 'rb')}
+
+            res = requests.post(url, files=files, params=params)
+
+            match res.status_code:
+                case 200:
+                    json_data = json.loads(res.text)
+                    print(200, "OK 上传文件", json_data["filename"])
+                    return json_data["id"]
+                case 400:
+                    print(400, "Invalid request")
+                    return None
+                case 401:
+                    print(401, "Unauthorized")
+                    return None
+                case 413:
+                    print(413, "File too large")
+                    return None
+                case 500:
+                    print(500, "Internal server error")
+                    return None
+                case _:
+                    print("Unknown error")
+                    return None
+                
+        except Exception as e:
+            print("upload file error:", e)
+            return None
+    
+    # 创建资源
+    def create_resource(self, fileUrl):
+        url = urljoin(self.openapi_url, API_VERSION + "resource")
+        # url = self.openapi_url + "resource"
+        headers = {"Content-Type": "application/json"}
+        data = {
+            "filename" : fileUrl.split("/")[-1],
+            "externalLink": fileUrl
+        }
+        params = {"openId" : self.openId}
+        try:
+            
+            res = requests.post(url, json=data, headers=headers, params=params)
+
+            match res.status_code:
+                case 200:
+                    print(200, "OK", res.content)
+                    return json.loads(res.content)["id"]
+                case 400:
+                    print(400, "Invalid request")
+                    return None
+                case 401:
+                    print(401, "Unauthorized")
+                    return None
+                case 413:
+                    print(413, "File too large")
+                    return None
+                case 500:
+                    print(500, "Internal server error")
+                    return None
+                case _:
+                    print("Unknown error")
+                    return None
+                
+        except Exception as e:
+            print("create resource error:", e)
+            return None
+
+if __name__ == "__main__":
+    pass
+        
